5032e54c32c0b65957d5f8f7dab1934eab9c1d43
LukeGus <bugattiguy527@gmail.com>
2025-12-11 23:59:27 -0600

    fix: HTTP issues on iOS

--- plugins/withIOSNetworkSecurity.js
+++ plugins/withIOSNetworkSecurity.js
@@ -7,50 +7,10 @@ const withIOSNetworkSecurity = (config) => {
     existingPlist.NSAppTransportSecurity = {
       NSAllowsArbitraryLoads: true,
       NSAllowsLocalNetworking: true,
-      NSAllowsArbitraryLoadsInWebContent: true,
-      NSAllowsArbitraryLoadsForMedia: true,
-      NSExceptionDomains: {
-        "localhost": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "127.0.0.1": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "0.0.0.0": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "192.168.0.0": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "10.0.0.0": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "172.16.0.0": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "100.64.0.0": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "169.254.0.0": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-        "fd00::": {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-      },
     };
 
     existingPlist.NSLocalNetworkUsageDescription =
-      "Termix needs to connect to servers on your local network for SSH and other services.";
+      "Termix needs to connect to servers to load hosts and initiate SSH connections";
 
     existingPlist.NSBonjourServices = ["_ssh._tcp", "_sftp-ssh._tcp"];
 
