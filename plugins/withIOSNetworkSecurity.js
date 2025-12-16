539251d591e5f32661008435979fdec9ab207f17
LukeGus <bugattiguy527@gmail.com>
2025-12-16 01:01:44 -0600

    fix: ios http

--- plugins/withIOSNetworkSecurity.js
+++ plugins/withIOSNetworkSecurity.js
@@ -1,20 +1,12 @@
-const { withInfoPlist, withDangerousMod } = require("@expo/config-plugins");
+const { withInfoPlist, withDangerousMod, IOSConfig } = require("@expo/config-plugins");
 const fs = require("fs");
-const path = require("path");
 
 const withIOSNetworkSecurity = (config) => {
   config = withInfoPlist(config, (config) => {
+    delete config.modResults.NSAppTransportSecurity;
+
     config.modResults.NSAppTransportSecurity = {
       NSAllowsArbitraryLoads: true,
-      NSAllowsArbitraryLoadsInWebContent: true,
-      NSAllowsLocalNetworking: true,
-      NSAllowsArbitraryLoadsForMedia: true,
-      NSExceptionDomains: {
-        localhost: {
-          NSExceptionAllowsInsecureHTTPLoads: true,
-          NSIncludesSubdomains: true,
-        },
-      },
     };
 
     config.modResults.NSLocalNetworkUsageDescription =
@@ -28,26 +20,19 @@ const withIOSNetworkSecurity = (config) => {
   config = withDangerousMod(config, [
     "ios",
     async (config) => {
-      const podfilePath = path.join(config.modRequest.platformProjectRoot, "Podfile");
+      const { platformProjectRoot } = config.modRequest;
 
-      if (fs.existsSync(podfilePath)) {
-        let podfileContent = fs.readFileSync(podfilePath, "utf8");
+      try {
+        let infoPlist = IOSConfig.InfoPlist.read(platformProjectRoot);
 
-        const atsConfig = `
-  post_install do |installer|
-    installer.pods_project.targets.each do |target|
-      target.build_configurations.each do |config|
-        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'RCT_DEV=1'
-      end
-    end
-  end
-`;
+        delete infoPlist.NSAppTransportSecurity;
 
-        if (!podfileContent.includes("post_install do |installer|")) {
-          podfileContent += atsConfig;
-          fs.writeFileSync(podfilePath, podfileContent);
-        }
+        infoPlist.NSAppTransportSecurity = {
+          NSAllowsArbitraryLoads: true,
+        };
+
+        IOSConfig.InfoPlist.write(platformProjectRoot, infoPlist);
+      } catch (e) {
       }
 
       return config;
